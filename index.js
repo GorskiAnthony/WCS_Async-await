2057d05ed28c6c0144b55bac02e1eb52f8c2f3ef
Anthony Gorski <[anthony.gorski@wildcodeschool.com](mailto:anthony.gorski@wildcodeschool.com)>
2023-04-21 15:28:42 +0200

    init generalite

---
+++ index.js
@@ -0,0 +1,39 @@
+// /**
+//  * Synchrone
+//  */
+// console.log("HTML");
+// console.log("CSS");
+// console.log("JS");
+
+// /**
+//  * Asynchrone
+//  */
+// console.log("HTML");
+// setTimeout(() => {
+// 	console.log("CSS");
+// }, 5000);
+// console.log("JS");
+
+/**
+ * fonction anonyme
+ *
+ * function() {}
+ * () => {}
+ */
+
+/**
+ * Callback
+ */
+
+// // Fonction sayHello qui prend en paramettre une string
+// const sayHello = (name) => {
+// 	return `Salut ${name}, tu vas bien ?`;
+// };
+
+// function introduction(firstname, lastname, callback) {
+// 	const fullname = `${firstname} ${lastname}`;
+// 	// j'appel ma fonction de callback
+// 	console.log(callback(fullname));
+// }
+
+// introduction("Anthony", "LeBG", sayHello);
